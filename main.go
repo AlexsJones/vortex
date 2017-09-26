73af91148ed2896424876323f74d2274f7391eba
AlexsJones <tibbar@freedommail.ch>
2017-09-26 19:11:58 +0100

    added stupid banner

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"html/template"
@@ -8,6 +9,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/dimiro1/banner"
 	"github.com/fatih/color"
 
 	yaml "gopkg.in/yaml.v2"
@@ -18,8 +20,21 @@ import (
 *     Created By          :     jonesax
 *     Creation Date       :     [2017-09-26 18:35]
 **********************************************************************************/
+const b string = `
+
+                  _
+                 | |
+ __   _____  _ __| |_ _____  __
+ \ \ / / _ \| '__| __/ _ \ \/ /
+  \ V / (_) | |  | ||  __/>  <
+   \_/ \___/|_|   \__\___/_/\_\
+
+
+`
 
 func main() {
+	banner.Init(os.Stdout, true, true, bytes.NewBufferString(b))
+
 	var t = flag.String("template", "", "path to template to populate")
 	var vars = flag.String("varpath", "", "path to var yaml to populate")
 	var output = flag.String("output", "", "name of output file")
