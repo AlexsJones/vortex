4e47597c533d3071cdbfa95ac8a9ebeade9a4604
AlexsJones <tibbar@freedommail.ch>
2017-09-26 18:58:47 +0100

    updated

---
+++ README.md
@@ -0,0 +1,12 @@
+# vortex
+
+A simple template reader and variable injector
+
+- Used for when you have a bunch of templates (e.g. kubernetes files) and want to inject a yaml file of variables
+
+## Usage
+
+```
+go run main.go -template example/demo.tmpl -output test.txt -varpath example/vars.yaml
+
+```
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"os"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+/*********************************************************************************
+*     File Name           :     main.go
+*     Created By          :     jonesax
+*     Creation Date       :     [2017-09-26 18:35]
+**********************************************************************************/
+
+func main() {
+	var t = flag.String("template", "", "path to template to populate")
+	var vars = flag.String("varpath", "", "path to var yaml to populate")
+	var output = flag.String("output", "", "name of output file")
+	flag.Parse()
+	//Parse template -------------------------------------
+	tout, err := template.ParseFiles(*t)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	//Create output file ---------------------------------
+	f, err := os.Create(*output)
+	if err != nil {
+		log.Println("create file: ", err)
+		return
+	}
+	defer f.Close()
+
+	//Read YAML ------------------------------------------
+	bytes, err := ioutil.ReadFile(*vars)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	m := make(map[string]string)
+	err = yaml.Unmarshal(bytes, m)
+	if err != nil {
+		log.Print("yaml: ", err)
+		return
+	}
+	//Execute template ----------------------------------
+	err = tout.Execute(f, m)
+	if err != nil {
+		log.Print("execute: ", err)
+		return
+	}
+
+	fmt.Println(tout)
+}
