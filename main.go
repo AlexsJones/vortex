17085bd7aeface35aa2015aac6e7c17feb278477
erleene <erleene@beamery.com>
2018-04-09 12:07:37 +0100

    Erleene/Jade - Add support for parsing a directory of templates, add more tests

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/fatih/color"
 
-	yaml "gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"
 	"path/filepath"
 )
 
@@ -24,87 +24,35 @@ var t *string
 var vars *string
 var output *string
 
-func exists(name string) error {
-	if _, err := os.Stat(name); err != nil {
-		panic(err)
+func main() {
+
+	t := flag.String("template", "", "path to template to populate")
+	vars := flag.String("varpath", "", "path to var yaml to populate")
+	output := flag.String("output", "", "name of output file")
+	flag.Parse()
+
+	if *t == "" || *vars == "" || *output == "" {
+		fmt.Println("vortex is a simple program to combine a template with a yaml file of defined varibles it uses golang {{.var}} format with standard yaml")
+		flag.Usage()
+		return
 	}
-	return nil
+
+	if err := InputParametersCheck(t, output, vars); err != nil {
+		log.Fatal(err)
+	}
+
+	if isDirectoryOfTemplates(*t) {
+		if err := ParseDirectoryTemplates(*t, *output, *vars); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		if err := ParseSingleTemplate(*t, *output, *vars); err != nil {
+			log.Fatal(err)
+		}
+	}
+
 }
 
-func isDir(name string) (bool, error) {
-	path, err := os.Stat(name)
-	if err != nil {
-		return false, err
-	}
-	if !path.IsDir() {
-		return false, nil
-	}
-	return true, nil
-}
-
-//template -- template/ output outout/ vars example/vars.yml
-//template is A DIRECTORY, and output IS A DIRECTORY
-// func parseDirectoryTemplates(tempName string, outName, string, vars string) {
-//
-// 	//read each file and parse with output file
-// 	//iterate through tempName files
-//
-// 	if files, err := ioutil.ReadDir(tempName); err != nil {
-// 		log.Println(err)
-// 		return
-// 	}else {
-// 		//loop each file and parse
-// 		for _, file := range files {
-// 			//
-// 			if tout, err := template.ParseFiles(file.Name()); err != nil {
-// 				log.Println(err)
-// 				return
-// 			}else {
-// 				//do something with tout
-// 				//create output file
-// 				var fileTo = file.Name()
-//
-// 				//apend .txt to fileTo
-// 				newFile := strings.Join(fileTo, ".txt")
-//
-// 				os.Chdir(outName)
-//
-// 				f, err := os.Create(newFile)
-// 				if err != nil {
-// 					log.Println("Create file: ", err)
-// 					return
-// 				}
-// 				defer f.Close()
-//
-// 				workDir := os.Getenv("PWD")
-//
-// 				os.Chdir(workDir)
-//
-// 			//Read YAML ------------------------------------------
-// 				bytes, err := ioutil.ReadFile(vars)
-// 				if err != nil {
-// 					log.Print(err)
-// 					return
-// 				}
-// 				m := make(map[string]interface{})
-// 				err = yaml.Unmarshal(bytes, m)
-// 				if err != nil {
-// 					log.Print("yaml: ", err)
-// 					return
-// 				}
-// 				//Execute template ----------------------------------
-// 				err = tout.Execute(f, m)
-// 				if err != nil {
-// 					log.Print("execute: ", err)
-// 					return
-// 				}
-// 				color.Green("Done")
-// 			}
-// 		}
-// 	}
-// }
-
-//template -- template/demo.tmpl output outout/ vars example/vars.yml
 func ParseSingleTemplate(tempName string, out string, vars string) error {
 	tout, err := template.ParseFiles(tempName)
 	if err != nil {
@@ -147,8 +95,58 @@ func ParseSingleTemplate(tempName string, out string, vars string) error {
 	return nil
 }
 
-//function to return if template is a directory - isTemplateDir(*t)
-func isTemplateDir(tempName string) bool {
+func ParseDirectoryTemplates(tempDirectory string, outDirectory string, vars string) error {
+	files, err := ioutil.ReadDir(tempDirectory)
+	if err != nil {
+		return err
+	}
+
+	for _, f := range files {
+		if err := ParseSingleTemplate(fmt.Sprint(tempDirectory, "/", f.Name()), outDirectory, vars); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func InputParametersCheck(t *string, output *string, v *string) error {
+	if err := exists(*t); err != nil {
+		return err
+	}
+
+	if err := exists(*v); err != nil {
+		return err
+	}
+	err := createOutputDirectoryIfDoesntExist(*output)
+	if err != nil {
+		return err
+
+	}
+
+	return nil
+}
+
+func exists(name string) error {
+	if _, err := os.Stat(name); os.IsNotExist(err) {
+		return err
+	}
+
+	return nil
+}
+
+func isDir(name string) (bool, error) {
+	path, err := os.Stat(name)
+	if err != nil {
+		return false, err
+	}
+	if !path.IsDir() {
+		return false, nil
+	}
+	return true, nil
+}
+
+func isDirectoryOfTemplates(tempName string) bool {
 	tempDir, err := isDir(tempName)
 	if err != nil {
 		log.Println(err)
@@ -157,81 +155,11 @@ func isTemplateDir(tempName string) bool {
 	return tempDir
 }
 
-//function to return if output is a directory - isOutputDir(*output)
-func isOutputDir(outName string) bool {
-	outDir, err := isDir(outName)
-	if err != nil {
-		log.Println(err)
-		return false
-	}
-	return outDir
-}
-
-func CreateOutputDirectoryIfDoesntExist(output string) error {
+func createOutputDirectoryIfDoesntExist(output string) error {
 	if _, err := os.Stat(output); os.IsNotExist(err) {
 		if err := os.Mkdir(output, 0700); err != nil {
 			return err
 		}
-	}
-
-	return nil
-}
-
-func main() {
-
-	t := flag.String("template", "", "path to template to populate")
-	vars := flag.String("varpath", "", "path to var yaml to populate")
-	output := flag.String("output", "", "name of output file")
-	flag.Parse()
-
-	if *t == "" || *vars == "" || *output == "" {
-		fmt.Println("vortex is a simple program to combine a template with a yaml file of defined varibles it uses golang {{.var}} format with standard yaml")
-		flag.Usage()
-		return
-	}
-
-	if err := checkForExistenceOfRequiredDirectories(t, output); err != nil {
-		log.Fatal(err)
-	}
-
-	// check if template(*t) is a file or a directory
-	tempDir := isTemplateDir(*t)
-
-	//check if *output is a directory
-	outDir := isOutputDir(*output)
-
-	//PARSE SINGLE FILE
-	switch tempDir {
-	case true:
-		if outDir {
-			//T is a directory, O is a directory;\s
-			//call function to parse template directory files and create each file for output
-			//parseDirectoryTemplates(*t, *output, *vars)
-			log.Println("Do nothing for now")
-		} else {
-			//output is not a directory, and template is a file- do nothing for now
-			log.Println("O file, and T is a file.Do nothing for now")
-		}
-	default:
-		//output is a directory, T is a file,  parse it
-		if outDir {
-			//call function to parse single template file
-			if err := ParseSingleTemplate(*t, *output, *vars); err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
-}
-
-func checkForExistenceOfRequiredDirectories(t *string, output *string) error {
-	if err := exists(*t); err != nil {
-		return err
-	}
-
-	err := CreateOutputDirectoryIfDoesntExist(*output)
-	if err != nil {
-		return err
-
 	}
 
 	return nil
--- main_test.go
+++ main_test.go
@@ -4,44 +4,60 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"fmt"
 	. "github.com/AlexsJones/vortex"
 	"io/ioutil"
 	"os"
-	"fmt"
 )
 
-var _ = Describe("Running the main application with valid parameters", func() {
-	Context("When running with an output directory that doesn't exist", func() {
+var _ = Describe("Running vortex with invalid parameters", func() {
+	templateFile := "test_files/test1.yaml"
+	invalidFile := "not-a-file"
+	varsFile := "test_files/vars.yaml"
+	testOutputDir := "this-dir-doesnt-exist"
+
+	Context("Without a template file", func() {
+		It("Should throw an error", func() {
+			err := InputParametersCheck(&invalidFile, &testOutputDir, &varsFile)
+			Expect(err).To(HaveOccurred())
+		})
+	})
+
+	Context("Without a variable file", func() {
+		It("Should throw an error", func() {
+			err := InputParametersCheck(&templateFile, &testOutputDir, &invalidFile)
+			Expect(err).To(HaveOccurred())
+		})
+	})
+})
+
+var _ = Describe("Running vortex with valid parameters", func() {
+	Context("With an output directory that doesn't exist", func() {
 		It("Should create the output directory", func() {
-			// Given I have a directory name that doesn't exist
-			// When I try to run vortex
-			// Then vortex will try to create the output directory for me
+			templateFile := "test_files/test1.yaml"
+			varsFile := "test_files/vars.yaml"
 			testOutputDir := "this-dir-doesnt-exist"
-			err := CreateOutputDirectoryIfDoesntExist(testOutputDir)
+			err := InputParametersCheck(&templateFile, &testOutputDir, &varsFile)
 			if err != nil {
 				Fail(err.Error())
 			}
 
 			Expect(testOutputDir).To(BeADirectory())
 
-			// Cleanup
 			os.RemoveAll(testOutputDir)
 		})
 	})
 
-	Context("When provide a template file, a variable file and an output directory", func() {
+	Context("With a template file, a variable file and an output directory", func() {
 		It("It should output the rendered template in the output directory", func() {
-			// Setup
 			testFiles := "test_files"
 			templateFile := "test1.yaml"
-			envFile := "env1.yaml"
+			varFile := "vars.yaml"
 			testOutput := "test_output"
 			os.MkdirAll(testOutput, 0700)
 
-			// Given + When
+			ParseSingleTemplate(fmt.Sprint(testFiles, "/", templateFile), testOutput, fmt.Sprint(testFiles, "/", varFile))
 
-			ParseSingleTemplate(fmt.Sprint(testFiles, "/", templateFile), testOutput, fmt.Sprint(testFiles, "/", envFile))
-			// Then
 			content, err := ioutil.ReadFile(fmt.Sprint(testOutput, "/", templateFile))
 			if err != nil {
 				Fail(err.Error())
@@ -50,7 +66,37 @@ var _ = Describe("Running the main application with valid parameters", func() {
 			expectedSubstring := "name: test-name"
 			Expect(string(content)).To(ContainSubstring(expectedSubstring))
 
-			// Cleanup
+			os.RemoveAll(testOutput)
+		})
+	})
+
+	Context("With a template directory, a variable file and an output directory", func() {
+		It("It should output the rendered templates in the output directory", func() {
+			testFiles := "test_files"
+			templateFiles := []string{"test1.yaml", "test2.yaml"}
+
+			varFile := "vars.yaml"
+			testOutput := "test_output"
+			os.MkdirAll(testOutput, 0700)
+
+			ParseDirectoryTemplates(testFiles, testOutput, fmt.Sprint(testFiles, "/", varFile))
+
+			content1, err := ioutil.ReadFile(fmt.Sprint(testOutput, "/", templateFiles[0]))
+			if err != nil {
+				Fail(err.Error())
+			}
+
+			expectedSubstring1 := "name: test-name"
+			Expect(string(content1)).To(ContainSubstring(expectedSubstring1))
+
+			content2, err := ioutil.ReadFile(fmt.Sprint(testOutput, "/", templateFiles[1]))
+			if err != nil {
+				Fail(err.Error())
+			}
+
+			expectedSubstring2 := "image: image-test"
+			Expect(string(content2)).To(ContainSubstring(expectedSubstring2))
+
 			os.RemoveAll(testOutput)
 		})
 	})
