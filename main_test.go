220937342b9f411ecbcf86b17969033da157b3f9
erleene <erleene@beamery.com>
2018-04-09 10:30:52 +0100

    Erleene/Jade - Introduce Ginkgo for testing and add first test for single template

--- main_test.go
+++ main_test.go
@@ -1,12 +1,34 @@
 package main_test
 
 import (
-	"testing"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 
-	_ "github.com/AlexsJones/vortex"
+	. "github.com/AlexsJones/vortex"
+	"io/ioutil"
+	"strings"
+	"os"
 )
 
-func TestMain(m *testing.M) {
+var _ = Describe("Running the main application with valid parameters", func() {
+	Context("When provide a template file, a variable file and an output directory", func() {
+		It("It should output the rendered template in the output directory", func() {
+			// Setup
+			os.MkdirAll("test_output", 0700)
 
-	m.Run()
-}
+			// Given + When
+			ParseSingleTemplate("test_files/test1.yaml", "test_output", "test_files/env1.yaml")
+			// Then
+			content, err := ioutil.ReadFile("test_output/test1.txt")
+			if err != nil {
+				Fail(err.Error())
+			}
+
+			expectedSubstring := "name: test-name"
+			Expect(strings.Contains(string(content), expectedSubstring))
+
+			// Cleanup
+			os.RemoveAll("test_output")
+		})
+	})
+})
